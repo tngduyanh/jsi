c6008d926353ce7f31510c965ff3e52473576cab
username github <email github>
2021-11-21 21:10:45 +0700

    nuuu

---
+++ l4/b1.js
@@ -0,0 +1,14 @@
+// let arr1 = [1,2,3,5]
+// let array1 = [...arr1]
+// let copyArr = Object.assign({}, arr1);
+
+// console.log(array1, copyArr);
+
+
+// let arrA = ["Hello", "Xin chào"]
+// let arrB = ["Bonjour", "Olá"]
+
+// let copyArray = Object.assign({}, arrA + arrB);
+// let ahihi = [...arrA,arrB]
+
+// console.log(copyArray, ahihi);
